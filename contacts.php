b75a977ed1b8bae286b2a352c04603fd4f4830e9
Vit <vitalii.gyliaiev@gmail.com>
2017-04-18 09:03:55 +0300

    Add files via upload

--- contacts.php
+++ contacts.php
@@ -7,7 +7,8 @@ include (__DIR__.'/include/header.php');
    <div class="content"><hr>
     <div class="text_cont"><img src="/img/pic3b.png" alt="">По любым вопросам обращайтесь по телефону или пишите на почту<img src="/img/pic3b.png" alt=""> </div><hr>
     <div class="tel2"><img src="/img/phone3.png"> 098 089 1000</div><div class="clear"></div>
-    <div class="text_2"><img src="/img/email3.png">GranitKoros@gmail.com</div>
+    <div class="text_2"><img src="/img/email3.png">Granit1122@mail.ru
+    </div>
     <div class="clear"></div>
 
        <div class="forma">
